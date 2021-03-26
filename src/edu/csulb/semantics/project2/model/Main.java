a12d5d81cc971f31cca47168e9509d46e856e0cb
Krutika Pathak <krutikapathak084@gmail.com>
2021-03-26 16:41:02 -0700

    Added output progress in console.

--- src/edu/csulb/semantics/project2/model/Main.java
+++ src/edu/csulb/semantics/project2/model/Main.java
@@ -11,12 +11,16 @@ import java.io.IOException;
  */
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("\n---Building Ontology---");
+		System.out.println("\n\n--Model creation in progress...");
 		Model model = new Model();
 		model.addClasses();
 		model.addDataAndObjectProperties();
 		model.addInstances();
 		try {
+			System.out.println("\n--Writing OWL file in progress...");
 			model.writeModelToFile();
+			System.out.println("\n\n---Crashes.owl file created!---");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
